96d7c3e5f5ba92a0cc24f39f735d996d96623b56
rossbickmore <rossbickmore@gmail.com>
2019-12-27 22:20:09 +0000

    FR user can see posts and create their own posts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [posts, setPosts] = useState([])
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+  const baseUrl = "http://localhost:3000/posts"
+  
+  useEffect( () => {
+    const getPosts = async () => {
+      const response = await fetch(baseUrl)
+      return response.json()
+    }
+    getPosts().then( data => setPosts(data))
+  })
+
+  const addPost = e => {
+    e.preventDefault()
+    const newPost = {
+      title: title,
+      content: content
+    }
+    const create = async (object) => {
+      const options = {
+        method: "POST",
+        headers: {'Content-Type': 'application/json;charset=utf-8'},
+        body: JSON.stringify(object)
+      }
+      const response = await fetch(baseUrl, options)
+      console.log(response)
+    }
+    create(newPost)
+    setPosts(posts.concat(newPost))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Message Board</h1>
+      <form onSubmit={addPost}>
+        Title
+        <input value={title} onChange={ ({target}) => setTitle(target.value)}/>
+        Content
+        <input value={content} onChange={ ({target}) => setContent(target.value)}/>
+        <button type="submit">Create post</button>
+      </form>
+      <div>
+        { posts && posts.map( post => (
+          <div>
+            <h4>{post.title}</h4>
+            <p>{post.content}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
