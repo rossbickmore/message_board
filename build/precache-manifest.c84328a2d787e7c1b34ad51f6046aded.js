7c752ca5475d27363fb7dd4407ad37164227ba88
rossbickmore <rossbickmore@gmail.com>
2019-12-30 14:32:15 +0000

    uibuild

---
+++ build/precache-manifest.c84328a2d787e7c1b34ad51f6046aded.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08607158332562ab5c51c7aa620e3fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576ede2dd54f4465363c",
+    "url": "/static/js/2.e1cb0c05.chunk.js"
+  },
+  {
+    "revision": "bc151049c394d339f67718af17309e27",
+    "url": "/static/js/2.e1cb0c05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7d5a1402b5b54a7a7c0",
+    "url": "/static/js/main.4c70060a.chunk.js"
+  },
+  {
+    "revision": "806208fdbaa3d3c0a0d1",
+    "url": "/static/js/runtime-main.3925e367.js"
+  }
+]);
